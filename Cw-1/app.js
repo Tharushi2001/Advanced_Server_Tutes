2d5b857aa943a26acfd306a65d09a1d378b3edfe
Tharushi Amarabandu <114868535+Tharushi2001@users.noreply.github.com>
2025-02-25 12:53:07 +0530

    countries details

---
+++ Cw-1/app.js
@@ -0,0 +1,20 @@
+// app.js
+const express = require('express');
+const dotenv = require('dotenv');
+const db = require('./db');
+const countriesRoutes = require('./routes/countries');
+const auth = require('./middleware/auth');
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+// Use the countries routes
+app.use('/api', countriesRoutes);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
