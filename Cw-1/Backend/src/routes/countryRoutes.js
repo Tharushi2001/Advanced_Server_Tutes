ac1b6322fb9a2e9456c97a3954da38bdabbb39fd
Tharushi Amarabandu <114868535+Tharushi2001@users.noreply.github.com>
2025-03-19 00:09:50 +0530

    Organize File structure

--- Cw-1/Backend/src/routes/countryRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const CountryController = require('../controllers/CountryController');
-const ApiKeyMiddleware = require('../middleware/ApikeyMiddleware');
-
-const router = express.Router();
-
-router.get('/', ApiKeyMiddleware.validateApiKey, CountryController.getCountries);
-module.exports = router;
