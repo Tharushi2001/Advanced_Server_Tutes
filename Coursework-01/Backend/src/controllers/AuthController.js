4ba614be0a5fea1151e758d6b37489a54c68354e
Tharushi Amarabandu <114868535+Tharushi2001@users.noreply.github.com>
2025-04-14 07:46:46 +0530

    docker

--- Coursework-01/Backend/src/controllers/AuthController.js
+++ Coursework-01/Backend/src/controllers/AuthController.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const UserDao = require('../dao/UserDao');
 const ApiKeyDao = require('../dao/ApiKeyDao');
